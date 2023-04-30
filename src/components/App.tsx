5546e07c3730faf5dd3208a67e4199c9d07105d3
daler0022 <daler_olimov_0055@mail.ru>
2023-04-30 19:00:55 +0500

    dynamicform edited

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,66 @@ import {AppRouter} from "./AppRouter";
 import Navigation from "./Navigation";
 import React, {useContext } from "react";
 import {AuthContext} from "../context";
+import { DynamicForm, Field } from "../utils/DynamicForm";
+import axiosClient from "../utils/axiosClient";
 
 
 
 function App() {
   const {isAuth, isLoading} = useContext(AuthContext)
-  
+  // const fields: Field[] = [
+  //   {
+  //     name: "email",
+  //     label: "Email",
+  //     type: "email",
+  //     placeholder: "email",
+  //     required: true,
+  //   },
+  //   {
+  //     name: "password",
+  //     label: "password",
+  //     type: "password",
+  //     placeholder: "password",
+  //     required: true,
+  //   },
+  //   {
+  //     name: "password_confirmation",
+  //     label: "Confirm Password",
+  //     type: "text",
+  //     placeholder: "password",
+  //     required: true,
+  //   },
+  //   {
+  //     name: "fruct",
+  //     label: "Select",
+  //     type: "select",
+  //     placeholder: "select",
+  //     required: true,
+  //     options: [
+  //       { value: 'apple', label: 'Apple' },
+  //       { value: 'banana', label: 'Banana' },
+  //       { value: 'orange', label: 'Orange' },
+  //       { value: 'grape', label: 'Grape' }
+  //     ]
+  //   },
+  // ];
+
+  // interface UserReg {
+  //   email: string;
+  //   password: string;
+  //   role: string;
+  // }
+
+  // const createUser = (values: UserReg) => {
+  //   console.log(values);
+    
+  //   // try {
+  //   //   axiosClient.post("register", values)
+  //   // } catch (error) {
+  //   //   console.error(error);
+            
+  //   // }
+  // }
   return (
     <>
       {
@@ -24,6 +78,9 @@ function App() {
         >
         <AppRouter />
       </div>
+      {/* <DynamicForm fields={fields} onSubmit={(v) => createUser(v)} open={true} handleClose={function (): void {
+        throw new Error("Function not implemented.");
+      } } service={undefined}/> */}
     </>
   )
 }
--- src/pages/Admin/CategoryPage.tsx
+++ src/pages/Admin/CategoryPage.tsx
@@ -1,19 +1,39 @@
 import {useEffect, useState} from 'react';
 import {useFetching} from "../../hooks/useFetching";
-import { Input } from '@mui/material';
+import { Grid, Input } from '@mui/material';
 import { categoryService } from '../../services/category.service';
 import Table from '../../components/UI/Tables/TableStickyHeader';
 import EditForm from '../../components/UI/Modals/EditForm';
 import EditCategoryForm from '../../components/UI/Modals/EditCategoryForm';
+import { DynamicForm, Field } from '../../utils/DynamicForm';
+import { Button } from '@material-ui/core';
 
 const CategoryPage = () => {
+    const [isOpen, setIsOpen] = useState(false);
     
+    const fields: Field[] = [{
+        name: "title",
+        label: "Название",
+        type: "text",
+        placeholder: "Введите название",
+        required: true
+    }]
+
+    const create = (values: any) => {
+        const response = categoryService.create(values);
+        setIsOpen(false);
+    }
+
     return (
+    <>
         <div className="table-container">
 
             <h1>
                 Категории   
             </h1>
+            <Grid marginBottom={2} item justifyContent="start" xs={4}>
+                <Button onClick={() => setIsOpen(true)} variant='contained'>Добавить</Button>
+            </Grid>
             <Table
                 service={categoryService}
                 headers={[
@@ -22,11 +42,15 @@ const CategoryPage = () => {
                         field: 'title',
                     }
                 ]}
-                // Form={EditCategoryForm}
+                Form={DynamicForm}
             />
+
+            <DynamicForm fields={fields} onSubmit={create} open={isOpen} 
+                handleClose={() => setIsOpen(false)} service={undefined} />
 
             {/* {errors && <h1>{errors?.message}</h1>} */}
         </div>
+    </>
     );
 };
 
